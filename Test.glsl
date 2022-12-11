1cacef46f0b383ead307f641b05a878006d3f48d
JohnLee <lijiang01001@163.com>
2022-12-11 16:54:01 +0800

    sdf sphere

--- Test.glsl
+++ Test.glsl
@@ -1,37 +1,60 @@
-#define PI 3.141592653
+#define TMIN 0.1
+#define TMAX 20.
+#define RAYMARCH_TIME 128
+#define PRECISION .001
+#define AA 3
 
-vec2 FixUV(in vec2 uv){
-    return 3. * (2. * uv - iResolution.xy) / min(iResolution.x,iResolution.y);
+vec2 fixUV(in vec2 c) {
+    return (2. * c - iResolution.xy) / min(iResolution.x, iResolution.y);
 }
 
-vec3 DrawGrid(in vec2 uv){
-    vec3 col = vec3(0.4);
-    vec2 grid = floor(mod(uv,2.));
-    if(grid.x == grid.y) col = vec3(.6);
-
-    col = mix(col, vec3(0.), 
-        smoothstep(1.1*fwidth(uv.x),fwidth(uv.x),abs(uv.x)));
-    col = mix(col, vec3(0.), 
-        smoothstep(1.1*fwidth(uv.y),fwidth(uv.y),abs(uv.y)));
-
-    return col;
+float sdfSphere(in vec3 p) {
+    return length(p - vec3(0., 0., 2.)) - 1.5;
 }
 
-float DrawSphere(in vec2 uv,in vec2 center, in float radius){
-    float val = sign(length(uv - center) - radius*0.99);
-    float val2 = -sign(length(uv - center) - radius);
-    val = val2 + val - 1.;
-    return clamp(val,0.,1.);
+float rayMarch(in vec3 ro, in vec3 rd) {
+    float t = TMIN;
+    for(int i=0;i<RAYMARCH_TIME && t<=TMAX;++i){
+        vec3 p = ro + rd * t;
+        float d = sdfSphere(p);
+        if(d < PRECISION) break;
+        t += d;
+    }
+    return t;
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    vec2 uv = FixUV(fragCoord);
+vec3 calcNormal(in vec3 p) {
+    const float h = 0.0001;
+    const vec2 k = vec2(1, -1);
+    return normalize(k.xyy * sdfSphere(p + k.xyy * h) +
+        k.yyx * sdfSphere(p + k.yyx * h) +
+        k.yxy * sdfSphere(p + k.yxy * h) +
+        k.xxx * sdfSphere(p + k.xxx * h));
+}
 
-    vec2 mpos = FixUV(iMouse.xy);
+vec3 render(vec2 uv) {
+    vec3 color = vec3(0.);
+    vec3 ro = vec3(0., 0., -1.5);
+    vec3 rd = normalize(vec3(uv, 0.) - ro);
+    float t = rayMarch(ro, rd);
+    if(t < TMAX) {
+        vec3 p = ro + t * rd;
+        vec3 n = calcNormal(p);
+        vec3 light = vec3(2. * cos(iTime - 2.0), 1., 2. * sin(iTime - 2.0) + 2.);
+        float dif = max(dot(normalize(light - p), n),0.);
+        color = dif * vec3(1.);
+    }
+    return color;
+}
 
-    vec3 col = mix(DrawGrid(uv), vec3(0.78,0.21,0.63), 
-        DrawSphere(uv,mpos,1.));
-
-    fragColor = vec4(col, 1.0);
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    vec3 color = vec3(0.);
+    for(int m = 0; m < AA; m++) {
+        for(int n = 0; n < AA; n++) {
+            vec2 offset = 2. * (vec2(float(m), float(n)) / float(AA) - .5);
+            vec2 uv = fixUV(fragCoord + offset);
+            color += render(uv);
+        }
+    }
+    fragColor = vec4(color / float(AA * AA), 1.);
 }
