d53febbd7158e633046eb28bcff241957f7e62f5
JohnLee <lijiang01001@163.com>
2022-12-18 17:29:55 +0800

    texture

--- Test.glsl
+++ Test.glsl
@@ -1,3 +1,6 @@
+#iChannel0 "file://earthmap.jpg"
+#iChannel0::WrapMode "Repeat"
+
 #define TMIN 0.1
 #define TMAX 20.
 #define RAYMARCH_TIME 128
@@ -41,14 +44,14 @@ vec3 render(vec2 uv) {
         vec3 p = ro + t * rd;
         vec3 n = calcNormal(p);
         vec3 light = vec3(2. * cos(iTime - 2.0), 1., 2. * sin(iTime - 2.0) + 2.);
-        float dif = max(dot(normalize(light - p), n),0.);
+        float dif = max(dot(normalize(light - p), n),0.) * 0.8 + 0.2;
         color = dif * vec3(1.);
     }
     return color;
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec3 color = vec3(0.);
+    /*vec3 color = vec3(0.);
     for(int m = 0; m < AA; m++) {
         for(int n = 0; n < AA; n++) {
             vec2 offset = 2. * (vec2(float(m), float(n)) / float(AA) - .5);
@@ -56,5 +59,9 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
             color += render(uv);
         }
     }
-    fragColor = vec4(color / float(AA * AA), 1.);
+    fragColor = vec4(color / float(AA * AA), 1.);*/
+    vec2 uv = fragCoord / iResolution.y;
+    //uv *= 3.;
+    vec3 col = texture(iChannel0, uv).rgb;
+    fragColor = vec4(col,1.);
 }
